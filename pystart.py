8e279004ea639dda19a1ce306d3c115679c32db2
Aaron Christianson <ninjaaron@gmail.com>
2016-03-26 15:44:02 -0500

    updating dotfiles

--- pystart.py
+++ pystart.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
-
 import sys, os
 import re
 import subprocess as sp
+import easyproc as ep
 from importlib import reload as rl
+from pyedpiper import sh, pype, fmt, s
+from collist import displayhook
 
+py = pype
 HOME = os.environ.get('HOME')
+sys.displayhook = displayhook
