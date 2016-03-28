5cbec30c7d7a8b7d8ae83877b28fec9e76ff5582
Aaron Christianson <ninjaaron@gmail.com>
2016-03-28 17:29:29 -0500

    updating dotfiles

--- pystart.py
+++ pystart.py
@@ -3,9 +3,11 @@ import re
 import subprocess as sp
 import easyproc as ep
 from importlib import reload as rl
-from pyedpiper import sh, pype, fmt, s
+from pyedpiper import Stream, fmt, s, m
 from collist import displayhook
 
+pype = Stream(interpolate=True)
 py = pype
+sh = py.sh
 HOME = os.environ.get('HOME')
 sys.displayhook = displayhook
