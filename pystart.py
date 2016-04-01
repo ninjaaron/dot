dd4cf513876a1cca4f54cd31061facc9730b0f7c
Aaron Christianson <ninjaaron@gmail.com>
2016-03-31 23:09:02 -0500

    updating dotfiles

--- pystart.py
+++ pystart.py
@@ -1,13 +1,26 @@
 import sys, os
 import re
+from importlib import reload as rl
 import subprocess as sp
 import easyproc as ep
-from importlib import reload as rl
-from pyedpiper import Stream, fmt, s, m
+from pyedpiper import pype, sh,  f, s, m
 from collist import displayhook
 
-pype = Stream(interpolate=True)
-py = pype
-sh = py.sh
-HOME = os.environ.get('HOME')
+HOME = os.environ['HOME']
+
+class Prompt:
+    def __init__(self):
+        from socket import gethostname
+        colors = ('\x1b[%dm' % (30 + i) for i in range(8))
+        cnames = ('bk', 'rd', 'gr', 'yl', 'bl', 'mg', 'cy', 'wh')
+        self.colors = dict(zip(cnames, colors))
+        self.colors['nr'] = '\x1b[0m'
+        self.colors['host'] = gethostname()
+
+    def __str__(self):
+        self.colors['cwd'] = os.getcwd().replace(HOME, '~')
+        return '{bl}{host}{nr}:{gr}{cwd}{nr}> '.format(**self.colors)
+
 sys.displayhook = displayhook
+py = pype
+#sys.ps1 = Prompt()
