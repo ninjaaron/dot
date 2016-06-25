f6f378c332e0c8c79aee7a5a59fb4fec5c9ec6a2
Aaron Christianson <ninjaaron@gmail.com>
2016-06-25 02:57:46 -0500

    updating dotfiles

--- bin/dirlog.sh
+++ bin/dirlog.sh
@@ -2,7 +2,8 @@ c() {
   local dir="$@"
   local log=~/.cache/dirlog.db
   if [[ ! -e $log ]]; then
-    sqlite3 $log 'CREATE TABLE dirs(path TEXT, name TEXT, time TEXT);'
+    sqlite3 $log \
+      'CREATE TABLE dirs(path TEXT PRIMARY KEY, name TEXT, time TEXT);'
   fi
 
   if [[ -n $dir ]] && [[ ${dir#..} == $dir ]]; then
@@ -12,8 +13,8 @@ c() {
 
   if cd $dir 2> /dev/null; then
     ls
-    sqlite3 $log "DELETE FROM dirs WHERE path = '$PWD';
-    INSERT INTO dirs VALUES('$PWD', '$(basename $PWD)', datetime('now'))"
+    sqlite3 $log "INSERT OR REPLACE into dirs
+                  VALUES('$PWD', '$(basename $PWD)', datetime('now'))"
   elif [[ -n $target ]]; then
     sqlite3 $log "UPDATE dirs SET time = datetime('now') WHERE path = '$target'"
     cd "$target" 2> /dev/null
