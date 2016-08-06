1a44f218274e0643f987aa83139d1adb86569191
Aaron Christianson <ninjaaron@gmail.com>
2016-08-06 02:32:52 -0500

    updating dotfiles

--- pystart.py
+++ pystart.py
@@ -1,12 +1,58 @@
 import sys, os
 import re
 import subprocess as sp
-import easyproc as ep
-#from pyedpiper import pype, sh, f, s, m
-from collist import displayhook
+from pprint import pprint
 from timeit import timeit
+from inspect import stack
 
-HOME = os.environ['HOME']
+local_mods = ('from collist import displayhook;sys.displayhook = displayhook',
+              'import easyproc as ep', 'import requests', 'import dirlog')
+for mod in local_mods:
+    try:
+        exec(mod)
+    except ImportError as e:
+        print(e)
+
+try:
+    from importlib import reload as rl
+except ImportError:
+    rl = reload
+
+
+class _Pipe:
+    def __init__(self, func):
+        self.func = func
+
+    def __or__(self, other):
+        return self.func(other)
+
+    def __ror__(self, other):
+        return self.func(other)
+
+    def __mul__(self, other):
+        return self.func(other)
+
+    def __getattr__(self, name):
+        return self.func(name)
+
+
+f = _Pipe(lambda s: s.format(**stack()[2][0].f_locals))
+c = _Pipe(lambda hint: os.chdir(dirlog.get_and_update(hint)))
+sh = _Pipe(lambda c: ep.grab(c))
+
+if 'ep' in globals():
+    ep.ProcStream.__repr__ = lambda self: self.str
+
+
+class LazyDict(dict):
+    "dict for people who are too lazy to type brackets and quotation marks"
+    self.__getattr__ = dict.__getitem__
+    self.__setattr__ = dict.__setitem__
+    self.__delattr__ = dict.__getitem__
+    def __dir__: return list(self)
+
+env = LazyDict(os.environ)
+
 
 class Prompt:
     def __init__(self):
@@ -16,10 +62,13 @@ class Prompt:
         self.colors['nr'] = '\x1b[0m'
 
     def __str__(self):
-        self.colors['cwd'] = os.getcwd().replace(HOME, '~')
+        self.colors['cwd'] = os.getcwd().replace(env.HOME, '~')
         return '{bl}{cwd}{gr}>{nr} '.format(**self.colors)
 
-sys.displayhook = displayhook
-# py = pype
-# sys.ps1 = Prompt()
-from importlib import reload as rl
+
+class Prompt2(Prompt):
+    def __str__(self):
+        return '.' * (len(str(sys.ps1))-15) + ' '
+
+sys.ps1 = Prompt()
+sys.ps2 = Prompt2()
