5e4a2e9985970ae4e442ad6fb1d65d52125dfb11
Aaron Christianson <ninjaaron@gmail.com>
2016-11-21 14:42:29 -0600

    updating dotfiles

--- bin/tempconv.py
+++ bin/tempconv.py
@@ -3,5 +3,7 @@ import sys
 
 c2f = lambda x: x * 9/5 + 32
 f2c = lambda x: (x - 32) * 5/9
+i2c = lambda x: x * 2.54
+c2i = lambda x: x / 2.54
 
 print("%0.1f" % globals()[sys.argv[0].split('/')[-1]](float(sys.argv[1])))
