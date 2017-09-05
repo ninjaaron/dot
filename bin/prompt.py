071c16e2527a4c2deaa1abdbac80a02ef8b5fd7d
Aaron Christianson <ninjaaron@gmail.com>
2017-09-05 11:31:49 +0200

    went back to building my prompt with python.

--- bin/prompt.py
+++ bin/prompt.py
@@ -6,35 +6,46 @@ if os.environ['USER'] == 'root':
     print("%F{yellow}%m%f:%F{red}%~%f# ")
     sys.exit()
 
+# prepare git stuff.
 branch_proc = sp.Popen(['git', 'branch'], stdout=sp.PIPE,
                        stderr=sp.DEVNULL, universal_newlines=True)
 status_proc = sp.Popen(['git', 'status', '-s'], stdout=sp.PIPE,
                        stderr=sp.DEVNULL, universal_newlines=True)
 
+# truncate the directory
 dir = os.getcwd().replace(os.environ['HOME'], '~').split('/')
 if len(dir) > 1:
-    short_dir = '/'.join(dir[:1]+[d[0] for d in dir[1:-1]]+dir[-1:])
+    short_dir = os.path.join(*(dir[:1]+[d[0] for d in dir[1:-1]]+dir[-1:]))
 else:
     short_dir = dir[0]
 
-prompt = '%F{{blue}}{}%f> '.format(short_dir)
+prompt = [f'%F{{blue}}{short_dir}%f> ']
 
+# print the host name over ssh
 if os.environ.get('SSH_TTY'):
-    prompt = '%F{{green}}%m%f:' + prompt
+    prompt.append('%F{green}%m%f:')
 
+# parse the git status info
 try:
     branch = [i for i in branch_proc.stdout if i.startswith('*')][0][2:-1]
     color = 'red' if status_proc.stdout.read() else 'green'
-    prompt = '%F{{{}}}{}%f|'.format(color, branch) + prompt
+    prompt.append(f'%F{{{color}}}{branch}%f|')
 except IndexError:
     pass
 
+# check about updates
 try:
     updates = open(os.environ['HOME']+'/.updates', 'rb').read().hex()
 except FileNotFoundError:
     pass
 else:
     if updates != '00':
-        prompt = '%F{{yellow}}{}%f|'.format(updates.lstrip()) + prompt
+        prompt.append(f'%F{{yellow}}{updates.lstrip()}%f|')
 
-print(prompt)
+# virtualenv stuff
+venv = os.environ.get('VIRTUAL_ENV')
+if venv:
+    prompt.append(f'{os.path.basename(venv)}|')
+
+
+print(''.join(prompt[::-1]))
