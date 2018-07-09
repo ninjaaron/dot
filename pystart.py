9110e8a3f905ea368052f8143f6f8b0f2b407561
Aaron Christianson <ninjaaron@gmail.com>
2018-07-09 09:01:08 +0200

    updating dotfiles

--- pystart.py
+++ pystart.py
@@ -1,11 +1,9 @@
-import sys, os, re, shlex, inspect, json
-import subprocess as sp
+import sys, os, re
 from pprint import pprint
 from timeit import timeit
-from glob import glob
+import libaaron
 
-local_mods = ('from collist import displayhook;sys.displayhook = displayhook',
-              'import easyproc as ep', 'import requests', 'import dirlog')
+local_mods = ('from collist import displayhook;sys.displayhook = displayhook')
 for mod in local_mods:
     try:
         exec(mod)
@@ -16,70 +14,6 @@ try:
     from importlib import reload as rl
 except ImportError:
     rl = reload
-
-
-class Trigger:
-    def __init__(self, func):
-        self.func = func
-
-    def __repr__(self):
-        self.func()
-        return ''
-
-    def __truediv__(self, other):
-        return self.func(other)
-
-    def __getattr__(self, name):
-        return self.func(name)
-
-    def __getitem__(self, name):
-        return self.func(name)
-
-
-class Cmd:
-    def __init__(self, cmd, mode=None):
-        self.cmd = shlex.split(cmd) if isinstance(cmd, str) else cmd
-        self.mode = mode
-
-    def __pos__(self):
-        return ep.grab(self.cmd)
-
-    def __neg__(self):
-        ep.run(self.cmd)
-
-    def __repr__(self):
-        -self
-        return 'None'
-
-    def __getitem__(self, value):
-        if isinstance(value, (int, slice)):
-            return (+self)[value]
-        return Cmd(self.cmd+glob(os.path.expanduser(value)))
-
-    def __getattr__(self, name):
-        if name.startswith('_'):
-            return Cmd(self.cmd+['-'+name[1:]])
-        return self[name]
-
-    def __call__(self, *args, **kwargs):
-        return ep.grab(self.cmd, *args, **kwargs)
-
-    def __iter__(self):
-        return iter(+self)
-
-    def __sub__(self, flag):
-        return self.F/flag
-
-    def __truediv__(self, value):
-        return self[value]
-
-    def __or__(self, func):
-        return tuple(map(func, +self))
-
-    def __dir__(self):
-        return ep.grab('ls').tuple
-
-
 # class Exit:
 #     def __repr__(self):
 #         __builtins__.exit()
@@ -88,43 +22,7 @@ class Cmd:
 # exit = Exit()
 
 
-def funky_junk():
-
-    @Trigger
-    def c(hint=None):
-        os.chdir(dirlog.get_and_update(hint))
-        -(sh/'ls --color=auto')
-
-    @Trigger
-    def vs(cmd):
-        if inspect.ismodule(cmd):
-            -(sh/env.PAGER/cmd.__file__)
-            return
-        what = +sh.which[cmd]
-        if what.len > 1:
-            ep.run([env.PAGER, '-c', 'set ft=sh'], stdin=what)
-        else:
-            sh/env.PAGER/what
-
-    f = Trigger(lambda s: s.format(**inspect.stack()[2][0].f_locals))
-    h = Trigger(lambda func=None: help() if func is None else help(func))
-    sh = Trigger(lambda c: Cmd(c))
-    ls, e, clear, view = sh/'ls --color=auto', sh.permedit, sh.clear, sh.vimpager
-    ll, la, lla = ls._l, ls._a, ls._la
-    rm = sh.rm._r
-    globals().update(locals())
-
-
-
-class DotDict(dict):
-    "dict for people who are too lazy to type brackets and quotation marks"
-    __getattr__ = dict.__getitem__
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
-    def __dir__(self): return list(self)
-
-
-env = DotDict(os.environ)
+env = libaaron.DotDict(os.environ)
 
 
 # class Prompt:
