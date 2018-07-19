d7f771f1ecadbc36ad049eda9321e089e2d57a4c
Aaron Christianson <ninjaaron@gmail.com>
2018-07-19 16:10:08 +0200

    updating dotfiles

--- bin/transmission.py
+++ bin/transmission.py
@@ -1,36 +1,40 @@
-import asyncio
+#!/usr/bin/env python3
 import json
-import functools
-import os
+import shlex
+import subprocess as sp
 import sys
+import typing as t
 import aiohttp
-import libaaron
 import compose
+import libaaron
+from libaaron import aio
 URL = 'http://localhost:9091/transmission/rpc'
 HEADER = 'X-Transmission-Session-Id'
 dct = libaaron.DotDict
 decoder = json.JSONDecoder(object_hook=dct)
+IDs = t.Union[t.Sequence[int], int]
+
+
+class Duplicate(Exception):
+    def __init__(self, tor):
+        self.tor = tor
+        self.args = tor.name,
+
 
 # base async api
-
-
-def run(coro):
-    # stole the idea from 3.7
-    loop = asyncio.get_event_loop()
-    return loop.run_until_complete(coro)
-
-
-def spawn(coro):
-    # also again stole the idea from 3.7
-    return asyncio.get_event_loop().create_task(coro)
-
-
 class AsyncSession:
     __slots__ = 'sess', 'hdr', 'url'
 
-    def __init__(self, url=URL):
+    def __init__(self, url: str = URL):
         self.url = url
         self.sess = aiohttp.ClientSession()
+
+    def __repr__(self):
+        name = self.__class__.__name__
+        if self.url == URL:
+            return name + '()'
+
+        return '%s(url=%r)' % (name, self.url)
 
     async def close(self):
         await self.sess.close()
@@ -49,7 +53,9 @@ class AsyncSession:
                 self.hdr = {HEADER: resp.headers[HEADER]}
             return self.hdr
 
-    async def request(self, method, arguments, tag=None):
+    async def request(
+            self, method: str, arguments: t.Mapping, tag: int = None
+    ) -> t.Mapping:
         data = dict(method=method, arguments=arguments)
         if tag:
             data['tag'] = tag
@@ -57,7 +63,13 @@ class AsyncSession:
                 URL, json=data, headers=await self.get_hdr()) as resp:
             return await resp.json(loads=decoder.decode)
 
-    async def torrents(self, method, arguments, ids=None, *args, **kwargs):
+    async def torrents(
+            self,
+            method: str,
+            arguments: t.Mapping,
+            ids: IDs = None,
+            *args, **kwargs
+    ) -> t.Mapping:
         if arguments is None:
             arguments = {}
         if ids:
@@ -65,7 +77,9 @@ class AsyncSession:
         method = 'torrent-' + method
         return await self.request(method, arguments, *args, **kwargs)
 
-    async def tget(self, fields, *args, **kwargs):
+    async def tget(
+            self, fields: t.Sequence[str], *args, **kwargs
+    ) -> t.List[t.Mapping]:
         if isinstance(fields, str):
             fields = [fields]
         else:
@@ -75,10 +89,12 @@ class AsyncSession:
         data = await self.torrents('get', arguments, *args, **kwargs)
         return data.arguments.torrents
 
-    async def tset(self, arguments, *args, **kwargs):
+    async def tset(
+            self, arguments: t.Mapping, *args, **kwargs
+    ) -> t.Mapping:
         return await self.torrents('set', arguments, *args, **kwargs)
 
-    async def tremove(self, ids, delete=False, *args, **kwargs):
+    async def tremove(self, ids: IDs, delete=False, *args, **kwargs):
         arguments = {'delete-local-data': delete}
         return await self.torrents('remove', arguments, ids, *args, **kwargs)
 
@@ -95,7 +111,13 @@ class AsyncSession:
         arguments['filename'] = filename
         arguments['paused'] = paused
         result = await self.torrents('add', arguments, *args, **kwargs)
-        return result.arguments['torrent-added']
+        try:
+            return result.arguments['torrent-added']
+        except KeyError:
+            if 'torrent-duplicate' in result.arguments:
+                raise Duplicate(result.arguments['torrent-duplicate'])
+            else:
+                raise
 
 
 @compose.struct
@@ -103,65 +125,112 @@ class Torrent:
     data: dict
     session: AsyncSession
 
+    @classmethod
+    def new(cls, id: int, session: AsyncSession):
+        return cls({'id': id}, session)
+
     async def add_attrs(self, *attrs):
         tor, = await self.session.tget(attrs, ids=self.id)
         self.data.update(tor)
 
     async def update(self):
-        await self.add_attrs(self.data)
+        await self.add_attrs(*self.data)
 
-    async def __getattr__(self, attr):
-        try:
-            return self.data[attr]
-        except KeyError:
-            tor, = await self.session.tget(attr, ids=self.id)
-            self.data.update(tor)
+    def __getattr__(self, attr):
         return self.data[attr]
 
     __getitem__ = __getattr__
 
+    async def get(self, attr):
+        try:
+            return self.data[attr]
+        except KeyError:
+            await self.add_attrs(attr)
+            return self.data[attr]
+
+    async def remove(self, delete=False):
+        return await self.session.tremove(self.id, delete)
+
+
+class Torrents:
+    __slots__ = 'session', 'queue'
+
+    async def __call__(
+            self, *ids: IDs, fields='name', url=None):
+        if not hasattr(self, 'session'):
+            self.session = AsyncSession(url or URL)
+        elif url:
+            self.session.close()
+            self.session = AsyncSession(url)
+
+        self.queue = [
+            Torrent(data, self.session) for data in
+            await self.session.tget(fields, ids=ids)]
+
+        return self
+
+    async def __aenter__(self):
+        return await self()
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.close()
+
+    async def close(self):
+        await self.session.close()
+
+    def __iter__(self):
+        return iter(self.queue)
+
+    async def add(self, filename, paused=False):
+        try:
+            tor = await self.session.tadd(filename, paused)
+            tor = Torrent(tor, self.session)
+            self.queue.append(tor)
+            return tor
+        except Duplicate as d:
+            return Torrent(d.tor, self.session)
+
+
+Torrent.s = Torrents()
+
+
+async def sftp(torrent, host, remove=False, delete=False, coro=False):
+    await torrent.add_attrs('percentDone', 'downloadDir', 'name')
+    while not torrent.percentDone == 1:
+        print(torrent.percentDone*100, torrent.name)
+        await aio.sleep(5)
+        await torrent.update()
+    args = [
+        'sftp', '-ar',
+        host + ':/' + shlex.quote(torrent.downloadDir[1:] + torrent.name),
+        '.'
+    ]
+
+    if coro:
+        with open(torrent.name + '.log', 'wb') as log:
+            proc = await aio.proc(*args, stderr=sp.STDOUT, stdout=log)
+        ret = await proc.wait()
+    else:
+        ret = sp.run(args).returncode
+
+    if ret == 0 and (remove or delete):
+        await torrent.remove(delete)
+
 
 async def main():
-    async with AsyncSession() as session:
-        tor = await session.tadd(sys.argv[1])
-        print(tor)
-        data = await session.tget(['name', 'downloadDir'])
-        print(*data, sep='\n')
-        for i in data:
-            if 'Taylor' in i.name:
-                await session.tremove(i.id, delete=True)
-                print('removed', i.name)
-        print(await session.tget('name'))
+    async with Torrent.s as ts:
+        fs = await ts.session.request(
+            'free-space', {'path': '/home/ninjaaron/'})
+        print(fs)
+        tor = await ts.add(sys.argv[1])
+        print(tor.id, tor.name, await tor.get('downloadDir'))
+        print(*ts)
+        dls = [await aio.spawn(sftp(t, 'sink', coro=True, delete=True))
+               for t in ts]
+        for dl in dls:
+            await dl
     return 'foo'
 
 
-run(main())
-
-
-# sychronous hacks. to be avoided.
-
-
-def get_coro_wrapper(method):
-    @functools.wraps(method)
-    def wrapper(self, *args, **kwargs):
-        return run(method(self.sess, *args, **kwargs))
-    return wrapper
-
-
-class Session:
-    def __init__(self, url=URL):
-        stderr = sys.stderr
-        with open(os.devnull, 'w') as devnull:
-            sys.stderr = devnull
-            self.sess = AsyncSession(url)
-        sys.stderr = stderr
-
-    def __enter__(self):
-        return self
-
-    __exit__ = get_coro_wrapper(AsyncSession.__aexit__)
-
-
-for k, v in AsyncSession.__dict__.items():
-    if callable(v) and not k.startswith('__'):
-        setattr(Session, k, get_coro_wrapper(v))
+if __name__ == '__main__':
+    aio.run(main())
