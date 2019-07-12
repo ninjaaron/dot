b79cd25a6decf9a58c7cdda2615f5d254e5e342b
Aaron Christianson <ninjaaron@gmail.com>
2019-07-12 17:03:53 +0200

    updating dotfiles

--- partial/fish/config.fish
+++ partial/fish/config.fish
@@ -4,3 +4,7 @@ function act
     set venv $argv[1]
     source $venv/bin/activate.fish
 end
+
+# opam configuration
+source /home/ninjaaron/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+set -x MANPATH $MANPATH /usr/share/man
