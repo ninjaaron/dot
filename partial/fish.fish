bf6d57544c0653edb5b48119ce148941e8a566fc
Aaron Christianson <ninjaaron@gmail.com>
2022-09-06 10:26:40 +0200

    updating dotfiles

--- partial/fish.fish
+++ partial/fish.fish
@@ -2,10 +2,10 @@
 
 cd fish
 set fishdir $HOME/.config/fish
-mkdir $fishdir ^ /dev/null
+mkdir $fishdir 2> /dev/null
 
 for name in config.fish functions
-    rm -r $fishdir/$name ^ /dev/null
+    rm -r $fishdir/$name 2> /dev/null
 end
 
 ln -s $PWD/config.fish $fishdir
