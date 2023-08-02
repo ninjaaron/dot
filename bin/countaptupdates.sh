d5dfccc14107e19714381c6c6210b1526d0dd25d
Aaron Christianson <ninjaaron@gmail.com>
2023-08-02 11:56:41 +0200

    updating dotfiles

--- bin/countaptupdates.sh
+++ bin/countaptupdates.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 upgradable="$(apt list --upgradable 2> /dev/null | wc -l)"
-expr "$upgradable" - 1 | $HOME/bin/i2b > $HOME/.updates
+echo $((upgradable - 1)) | "$HOME/bin/i2b" > "$HOME/.updates"
